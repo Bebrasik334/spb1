1787cc814137add61b1b029492e671a33dff3e57
spb <130841856+Bebrasik334@users.noreply.github.com>
2026-04-19 12:30:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const map = L.map('map').setView([59.935, 30.335], 13);
+// создание карты
+const map = L.map('map');
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
+
+// кастомный маркер с номером
 function createMarker(number){
     return L.divIcon({
         className:'marker',
@@ -13,7 +16,10 @@ function createMarker(number){
     });
 }
 
+
+// точки маршрута
 const points = [
+
 {coords:[59.9316,30.3565],name:"Улица Джона Леннона"},
 {coords:[59.9376,30.3483],name:"Подписные издания"},
 {coords:[59.9378,30.3478],name:"Памятник потерянной книге"},
@@ -21,34 +27,51 @@ const points = [
 {coords:[59.9415,30.3534],name:"Особняк Небылица"},
 {coords:[59.9441,30.3463],name:"Мозаичный дворик"},
 {coords:[59.9358,30.3299],name:"Sokol Coffee"},
+
+// исправленная Думская башня
 {coords:[59.9340,30.3293],name:"Думская башня"},
+
 {coords:[59.9389,30.3084],name:"Зеркальный дворик"}
+
 ];
 
+
+// добавление маркеров
 points.forEach((point,index)=>{
 
     const marker = L.marker(point.coords,{
         icon:createMarker(index+1)
     }).addTo(map);
 
+    // всплывающее название
     marker.bindPopup(`<b>${point.name}</b>`);
 
+    // переход в Яндекс.Карты
     marker.on("click", function(){
 
         const lat = point.coords[0];
         const lon = point.coords[1];
 
-        const url = `https://yandex.ru/maps/?ll=${lon},${lat}&z=17`;
+        // ссылка с меткой
+        const url = `https://yandex.ru/maps/?pt=${lon},${lat},pm2rdm&z=17`;
 
         window.open(url,"_blank");
-
     });
 
 });
 
+
+// линия маршрута
 const route = points.map(p=>p.coords);
 
-L.polyline(route,{
+const polyline = L.polyline(route,{
     color:"#ef4444",
     weight:4
 }).addTo(map);
+
+
+// авто-подгон карты под маршрут
+map.flyToBounds(polyline.getBounds(),{
+    padding:[50,50],
+    duration:1.5
+});
