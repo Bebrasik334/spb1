c07e0e655492b9405a3fcd30d4b3fd721be5b5ff
spb <130841856+Bebrasik334@users.noreply.github.com>
2026-04-19 12:23:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-
-// создание круглых маркеров с номером
 function createMarker(number){
     return L.divIcon({
         className:'marker',
@@ -15,70 +13,26 @@ function createMarker(number){
     });
 }
 
-
-// точки маршрута
 const points = [
-
-{
-coords:[59.9316,30.3565],
-name:"Улица Джона Леннона"
-},
-
-{
-coords:[59.9376,30.3483],
-name:"Подписные издания"
-},
-
-{
-coords:[59.9378,30.3478],
-name:"Памятник потерянной книге"
-},
-
-{
-coords:[59.9384,30.3468],
-name:"Фонтанный дом Анны Ахматовой"
-},
-
-{
-coords:[59.9415,30.3534],
-name:"Особняк Небылица"
-},
-
-{
-coords:[59.9441,30.3463],
-name:"Мозаичный дворик"
-},
-
-{
-coords:[59.9358,30.3299],
-name:"Sokol Coffee"
-},
-
-// ✅ исправленная Думская башня
-{
-coords:[59.9340,30.3293],
-name:"Думская башня"
-},
-
-{
-coords:[59.9389,30.3084],
-name:"Зеркальный дворик"
-}
-
+{coords:[59.9316,30.3565],name:"Улица Джона Леннона"},
+{coords:[59.9376,30.3483],name:"Подписные издания"},
+{coords:[59.9378,30.3478],name:"Памятник потерянной книге"},
+{coords:[59.9384,30.3468],name:"Фонтанный дом Анны Ахматовой"},
+{coords:[59.9415,30.3534],name:"Особняк Небылица"},
+{coords:[59.9441,30.3463],name:"Мозаичный дворик"},
+{coords:[59.9358,30.3299],name:"Sokol Coffee"},
+{coords:[59.9340,30.3293],name:"Думская башня"},
+{coords:[59.9389,30.3084],name:"Зеркальный дворик"}
 ];
 
-
-// добавление маркеров
 points.forEach((point,index)=>{
 
     const marker = L.marker(point.coords,{
         icon:createMarker(index+1)
     }).addTo(map);
 
-    // всплывающее название
     marker.bindPopup(`<b>${point.name}</b>`);
 
-    // переход в Яндекс.Карты по координатам
     marker.on("click", function(){
 
         const lat = point.coords[0];
@@ -92,8 +46,6 @@ points.forEach((point,index)=>{
 
 });
 
-
-// линия маршрута
 const route = points.map(p=>p.coords);
 
 L.polyline(route,{
